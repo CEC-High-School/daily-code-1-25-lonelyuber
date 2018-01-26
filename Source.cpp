67d83ca6e5b8382ab0fa4b6735b2a71ab7941d82
lonelyuber <35495442+lonelyuber@users.noreply.github.com>
2018-01-26 08:26:24 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,21 @@
+#include <iostream>
 
+using namespace std;
+char firstletter;
+char secondletter;
+char thirdletter;
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+int main() {
+	
+
+	cout << "What is the 3 character password?" << endl;
+	cin >> firstletter;
+	cin >> secondletter;
+	cin >> thirdletter;
+
+	if (firstletter == 'N' && secondletter == 'E' && thirdletter == 'S')
+		cout << "Welcome to the game" << endl;
+	else
+		cout << "next time buddy :)))" << endl;
+
+}
